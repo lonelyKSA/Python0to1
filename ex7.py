47ea1faaf2b856230aefaff08754733d034c468d
lonelyKSA <wxchforwork@163.com>
2016-10-13 02:20:49 +0800

    Add files via upload

---
+++ ex7.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+打印前面的部分并以字符串的格式输出’snow‘这个字符串
+print "And everywhere that Mary went."
+print "." * 10 # what'd that do? #将引号中间的部分打印十次
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+print end1 + end2 + end3 +end4 + end5 +end6, 
+# 逗号的作用在于把上下两行的内容连接为一行。顺利表达意思的同时保证美观
+print end7 +end8 + end9 + end10 +end11 +end12
+ 
